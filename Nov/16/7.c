b4f002f82fc548aaef423a0fb10615f4d973e254
pallavi-pandey <pallavipandey181@gmail.com>
2025-11-18 11:48:06 +0530

    removed

--- Nov/16/7.c
+++ Nov/16/7.c
@@ -1,17 +1,1 @@
 // Take a number as input and if that number is ODD or Even.
-
-#include<stdio.h>
-
-int main(){
-    int value;
-
-    printf("plese the your number: ");
-    scanf("%d",&value);
-
-    if(value/2==0){
-        printf("the number is EVEN");
-    } else{
-        printf("the number is ODD");
-    }
-    return 0;
-}
